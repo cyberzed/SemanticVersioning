e59aa63bd39b85b00ccb71582990ba285e3a3d46
Stefan D. Poulsen <cyberzed@sleddog.dk>
2012-09-05 20:34:25 +0200

    Cleanup the interactiontest

--- src/Sleddog.SemanticVersioning.Tests/InteractionTest.cs
+++ src/Sleddog.SemanticVersioning.Tests/InteractionTest.cs
@@ -10,29 +10,19 @@ namespace SemanticVersioning.Tests
 		[Fact]
 		public void SortWithComparer()
 		{
-			var expected = new List<SemanticVersion>();
-
-			var normal4 = new SemanticVersion(1, 2, 2);
-			var preRelease1 = new SemanticVersion(1, 2, 3, new[] {"aa"}, SemanticVersionType.PreRelease);
-			var preRelease3 = new SemanticVersion(1, 2, 3, new[] {"bb"}, SemanticVersionType.PreRelease);
-			var normal1 = new SemanticVersion(1, 2, 3);
-			var build1 = new SemanticVersion(1, 2, 3, new[] {"aa"}, SemanticVersionType.Build);
-			var build3 = new SemanticVersion(1, 2, 3, new[] {"bb"}, SemanticVersionType.Build);
-			var normal3 = new SemanticVersion(1, 2, 4);
-			var preRelease2 = new SemanticVersion(1, 3, 3, new[] {"aa"}, SemanticVersionType.PreRelease);
-			var normal2 = new SemanticVersion(1, 3, 3);
-			var build2 = new SemanticVersion(1, 3, 3, new[] {"aa"}, SemanticVersionType.Build);
-
-			expected.Add(normal4);
-			expected.Add(preRelease1);
-			expected.Add(preRelease3);
-			expected.Add(normal1);
-			expected.Add(build1);
-			expected.Add(build3);
-			expected.Add(normal3);
-			expected.Add(preRelease2);
-			expected.Add(normal2);
-			expected.Add(build2);
+			var expected = new List<SemanticVersion>
+			               	{
+			               		new SemanticVersion(1, 2, 2),
+			               		new SemanticVersion(1, 2, 3, new[] {"aa"}, SemanticVersionType.PreRelease),
+			               		new SemanticVersion(1, 2, 3, new[] {"bb"}, SemanticVersionType.PreRelease),
+			               		new SemanticVersion(1, 2, 3),
+			               		new SemanticVersion(1, 2, 3, new[] {"aa"}, SemanticVersionType.Build),
+			               		new SemanticVersion(1, 2, 3, new[] {"bb"}, SemanticVersionType.Build),
+			               		new SemanticVersion(1, 2, 4),
+			               		new SemanticVersion(1, 3, 3, new[] {"aa"}, SemanticVersionType.PreRelease),
+			               		new SemanticVersion(1, 3, 3),
+			               		new SemanticVersion(1, 3, 3, new[] {"aa"}, SemanticVersionType.Build)
+			               	};
 
 			var actual = new List<SemanticVersion>(expected);
 
